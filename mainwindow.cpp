ee0146d342e69fc4299b99e8c1f25036f43c0de1
OldBoots <oleg.vezuviy@mail.ru>
2022-03-07 11:30:56 +0700

    Test 1

---
+++ mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QGraphicsRectItem>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QGraphicsScene *scene = new QGraphicsScene;
+    QGraphicsRectItem *rect = new QGraphicsRectItem;
+    scene->setSceneRect(0,0,500,500);
+    rect->setBrush(Qt::blue);
+    rect->setRect(0,0,150,150);
+    scene->addItem(rect);
+    ui->graphicsView->setScene(scene);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
